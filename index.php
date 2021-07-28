7a4a61137afc91cfa3697da26c0ff7e514db8098
Josuedhc <josuedavidhc09@gmail.com>
2021-07-28 15:56:05 -0500

    Se agrega select2 para busqueda, menú y se quito de momento la funcion de java script

--- index.php
+++ index.php
@@ -21,47 +21,62 @@ $row=mysqli_fetch_array($query);
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
         <link rel="" href="https://cdn.datatables.net/fixedheader/3.1.6/css/fixedHeader.dataTables.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
         <link rel="stylesheet" href="styles.css" type="text/css" media="all">
         <title>Consultas</title>
     </head>
 
     <body>
-   
-    
+      <header>
+          <nav>
+          <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link" id="menu-1" aria-current="page" href="#consulta">Consulta</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="menu-2" href="#Cargar" >Cargar información</a>
+                    </li>
+            </ul>
 
-        <ul class="nav justify-content-end">
-            <li class="nav-item">
-                <a class="nav-link" id="menu-1" aria-current="page" href="#" onclick="mostrar('consulta');">Consulta</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="menu-2" href="#" onclick="mostrar('Cargar'); ">Cargar información</a>
-            </li>
-        </ul>
+          </nav> 
+        </header>
+        
         <!-- SECCION PARA CONSULTAR  -->
-        <section id="consulta" style="display:none;">
+       <!-- <section id="consulta" style="display:none;"> -->
+       <section id="consulta" style="display:block;"> 
             <form>
                 <div class="mb-3">
                     <h2>Folio</h2>
-                    <select class="form-select form-select-lg mb-3">
-                    
-                    <?php 
-                            $sql1 = "SELECT folio FROM siaf.comprometido group by folio;";
-                            $query1 = $conn -> query ($sql1);
-                        while($valores = mysqli_fetch_array($query1)){
-                            echo "<option value='".$valores['folio']."'>".$valores['folio']."</option>";
-                     }
-                        ?>
-        </select>
+                    <select id="seleBusca" class="form-select form-select-lg mb-3">
+                        
+                        <?php 
+                                $sql1 = "SELECT folio FROM siaf.comprometido group by folio;";
+                                $query1 = $conn -> query ($sql1);
+                            while($valores = mysqli_fetch_array($query1)){
+                                echo "<option value='".$valores['folio']."'>".$valores['folio']."</option>";
+                        }
+                            ?>
+                     </select>
                 </div>
                 <div class="d-grid gap-2 col-6 mx-auto">
                     <button type="submit" class="btn btn-primary">Consultar</button>
                 </div>
-            </form>
-            <table id="tablaCon" class="table table-striped">
+            </form> 
+            <div class="container-fluid">
+        <table id="tablaCon" class="display dataTable_width_auto">
                     <thead>
                         <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Clave presupuestal</th>
+                        <th scope="col">Ejercicio</th>
+                        <th scope="col">Ramo</th>
+                        <th scope="col">Institución</th>
+                        <th scope="col">Unidad</th>
+                        <th scope="col">Programa</th>
+                        <th scope="col">Iden Gasto</th>
+                        <th scope="col">Financiamiento</th>
+                        <th scope="col">Origen</th>
+                        <th scope="col">Procedencia</th>
+                        <th scope="col">Objeto Gasto</th>
                         <th scope="col">Total</th>
                         <th scope="col">Enero</th>
                         <th scope="col">Febrero</th>
@@ -84,7 +99,16 @@ $row=mysqli_fetch_array($query);
                         ?>
                         <tr>
                             <th><?php echo trim($row['idCompromiso'])?></th>
-                            <th><?php echo trim($row['ClavePresupuestal']) ?></th>
+                            <th><?php echo substr(($row['ClavePresupuestal']),0,2) ?></th>
+                            <th><?php echo substr(($row['ClavePresupuestal']),3,2) ?></th>
+                            <th><?php echo substr(($row['ClavePresupuestal']),6,3) ?></th>
+                            <th><?php echo substr(($row['ClavePresupuestal']),10,4) ?></th>
+                            <th><?php echo substr(($row['ClavePresupuestal']),21,4) ?></th>
+                            <th><?php echo substr(($row['ClavePresupuestal']),33,1) ?></th>
+                            <th><?php echo substr(($row['ClavePresupuestal']),35,1) ?></th>
+                            <th><?php echo substr(($row['ClavePresupuestal']),37,1) ?></th>
+                            <th><?php echo substr(($row['ClavePresupuestal']),39,3) ?></th>
+                            <th><?php echo substr(($row['ClavePresupuestal']),47,4) ?></th>
                             <th><?php echo trim($row['Total']) ?></th>
                             <th><?php echo $row['ENERO'] ?></th>
                             <th><?php echo $row['FEBRERO'] ?></th>
@@ -104,10 +128,19 @@ $row=mysqli_fetch_array($query);
                             }
                     ?>
                     </tbody>
-            </table>
+            </table>           
+
+        </div> 
         </section>
+
+
+
+
+
+
+
         <!-- SECCION PARA SUBIR LOS DATOS -->
-        <section id="Cargar" style="display:none;">
+        <section id="Cargar" style="display:block;">
             <div class="container">
                 <h2>Cargar e importar archivo excel a MySQL</h2>
                 <form name="importa" method="post" action="" enctype="multipart/form-data">
@@ -129,16 +162,128 @@ $row=mysqli_fetch_array($query);
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/fixedheader/3.1.6/js/dataTables.fixedHeader.min.js"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script>
      let temp = $("#btn1").clone();
         $("#btn1").click(function(){
-         $("#btn1").after(temp);
+         $("#btn1").after(temp);  
         });
 
         $(document).ready(function(){
              var table = $('#tablaCon').DataTable({
              orderCellsTop: true,
-             fixedHeader: true 
+             fixedHeader: true,
+             bAutoWidth: false,
+             language: {
+                search: "Filtrar:",  
+                info: "Mostrando desde el _START_ al _END_ del total de _TOTAL_ registros",
+                infoEmpty: "Mostrando desde el 0 al 0 del total de  0 registros", 
+                lengthMenu: "Mostrar _MENU_ registros por página",
+                paginate: {
+                    first: "Primera",
+                    last: "Última",
+                    next: "Siguiente",
+                    previous: "Anterior",
+                }
+             },
+             columnDefs: [{
+                    width: "10px",
+                    targets: 0
+                },
+                {
+                    width: "10px",
+                    targets: 1
+                },
+                {
+                    width: "10px",
+                    targets: 2
+                },
+                {
+                    width: "15px",
+                    targets: 3
+                },
+                {
+                    width: "15px",
+                    targets: 4
+                },
+                {
+                    width: "15px",
+                    targets: 5
+                },
+                {
+                    width: "15px",
+                    targets: 6
+                },
+                {
+                    width: "70px",
+                    targets: 7
+                },
+                {
+                    width: "70px",
+                    targets: 8
+                },
+                {
+                    width: "70px",
+                    targets: 9
+                },
+                {
+                    width: "70px",
+                    targets: 10
+                },
+                {
+                    width: "70px",
+                    targets: 11
+                },
+                {
+                    width: "70px",
+                    targets: 12
+                },
+                {
+                    width: "70px",
+                    targets: 13
+                },
+                {
+                    width: "70px",
+                    targets: 14
+                },
+                {
+                    width: "70px",
+                    targets: 15
+                },
+                {
+                    width: "70px",
+                    targets: 16
+                },
+                {
+                    width: "70px",
+                    targets: 17
+                },
+                {
+                    width: "70px",
+                    targets: 18
+                },
+                {
+                    width: "70px",
+                    targets: 19
+                },
+                {
+                    width: "70px",
+                    targets: 20
+                },
+                {
+                    width: "70px",
+                    targets: 21
+                },
+                {
+                    width: "70px",
+                    targets: 22
+                },
+                {
+                    width: "70px",
+                    targets: 23
+                }
+                ]
+             
         });
 
     //Creamos una fila en el head de la tabla y lo clonamos para cada columna
@@ -156,7 +301,12 @@ $row=mysqli_fetch_array($query);
                     .draw();
                  }
           } );
-         } );   
+        
+        } );   
+    });
+   
+    $(document).ready(function() {
+    $('#seleBusca').select2();
     });
 
     </script>
--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
-function mostrar(id){
-  var element = document.getElementById(id);
+function mostrar(id) {
+    var element = document.getElementById(id);
 
-  if(element.style.display == 'none'){
-    element.style.display = 'block';
-    
-  }else{
-    element.style.display = 'none';
-  
+    if (element.style.display == 'none') {
+        element.style.display = 'block';
 
-  }
+    } else {
+        element.style.display = 'none';
+
+
+    }
 }
 
-function valida(){
-  if (document.getElementById('menu-1').onclick()){
-    //document.getElementById('Cargar').style.display = 'block';
-    console.log("entro 1");
-  }else{
-    document.getElementById('consulta').style.display = 'none';
-    console.log("entro 2");
+function valida() {
+    if (document.getElementById('menu-1').onclick()) {
+        //document.getElementById('Cargar').style.display = 'block';
+        console.log("entro 1");
+    } else {
+        document.getElementById('consulta').style.display = 'none';
+        console.log("entro 2");
 
-  }
+    }
 }
